431897d08c6fdf698d887f1c0426e21940a84832
akdavila2 <anakarina.daviladavila@gmail.com>
2022-01-11 13:14:23 -0600

    creacion router

---
+++ my-app/src/components/AddCategory.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function AddCategory() {
+    return (
+        <div>
+            <h1>AddCategory</h1>
+        </div>
+    )
+}
+
+export default AddCategory
---
+++ my-app/src/lib/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { signInWithEmailAndPassword, getAuth, signOut } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCEvdtEX4tnm7sX90UzK-tuDyKu6vRnw4",
+  authDomain: "totallook-609cb.firebaseapp.com",
+  projectId: "totallook-609cb",
+  storageBucket: "totallook-609cb.appspot.com",
+  messagingSenderId: "336438149732",
+  appId: "1:336438149732:web:30168d3445c18385d91571",
+};
+
+// Initialize Firebase
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
+
+export const login = async (email, password) => {
+ return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const logout = () => signOut(auth);
