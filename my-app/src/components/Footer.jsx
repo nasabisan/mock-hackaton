cdc0f880935db4e551aca561c66b3e910ef20971
Nohely Badillo <62182593+nohelyx-bc@users.noreply.github.com>
2022-01-12 09:05:01 -0600

    estilos de footer

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-
+import React from "react";
 
 function Footer() {
-    return (
-        <div>
-             <div class="container">
-    <a class="navbar navbar-dark bg-dark" href="#">
-     <p> © 2022, Totallook All rights reserved. </p>
-    </a>
-  </div>
-        </div>
-    )
+  return (
+    <div>
+      <div class="footer-container">
+        <a class="navbar navbar-dark bg-dark" href="#">
+          <p> © 2022, Totallook All rights reserved. </p>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
