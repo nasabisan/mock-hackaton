70a8809bec98d256f8ae14c6182bf2571ab19508
AngelicaJH <isc.florjardinez@gmail.com>
2022-01-11 22:46:41 -0600

    Se creo Componente category

--- my-app/src/components/Categories.jsx
+++ my-app/src/components/Categories.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import img from '../assets/blusa.png'
 
-const Categories= ()=> {
+const Categories = () => {
+    const categories=["uno", "Dos", "Tres"];
     return (
         <div>
+
+            <section className='categories'>
+               
+                <div className='category-card'>
+                <img className='categories-image' alt='category image' src={img} />
+                <p className='category-title'>titulo categ  oria</p>
+                </div>
+                
+            </section>
+            {/* {
+                   for(let i=0; i==5;i++){
+                    console.log('hola')
+                   }
+               } */}
             <h1>Categories</h1>
         </div>
     )
