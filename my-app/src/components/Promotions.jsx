b442c3b97cbedd2961a9d577f6d0b2c00204f6a2
Nohely Badillo <62182593+nohelyx-bc@users.noreply.github.com>
2022-01-12 14:23:07 -0600

    estilos de promociones

--- my-app/src/components/Promotions.jsx
+++ my-app/src/components/Promotions.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import Header from './Header/Header';
+import React from "react";
+import Header from "./Header/Header";
+import promotion1 from "../assets/promotion1.png";
+import promotion2 from "../assets/promotion2.png";
+import footer from "../components/Footer";
 
-const Promotions =()=> {
-    return (
-        <div>
-            <Header />
-            <h1>View all Promotions</h1>
-        </div>
-    )
-}
+const Promotions = () => {
+  return (
+    <div>
+      <Header />
+      <h1>View all categories >></h1>
+      <div className="promotion-container">
+        <img src={promotion1} className="promotion-img" />
+        <img src={promotion2} className="promotion-img" />
+      </div>
+      <footer />
+    </div>
+  );
+};
 
-export default Promotions
+export default Promotions;
