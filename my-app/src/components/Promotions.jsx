515b8c398b81b371dd1b220ec074b1bafaacae03
Nohely Badillo <62182593+nohelyx-bc@users.noreply.github.com>
2022-01-12 14:50:31 -0600

    vista de promotions

--- my-app/src/components/Promotions.jsx
+++ my-app/src/components/Promotions.jsx
@@ -2,18 +2,16 @@ import React from "react";
 import Header from "./Header/Header";
 import promotion1 from "../assets/promotion1.png";
 import promotion2 from "../assets/promotion2.png";
-import footer from "../components/Footer";
 
 const Promotions = () => {
   return (
     <div>
       <Header />
-      <h1>View all categories >></h1>
+      <h1>View all categories</h1>
       <div className="promotion-container">
         <img src={promotion1} className="promotion-img" />
         <img src={promotion2} className="promotion-img" />
       </div>
-      <footer />
     </div>
   );
 };
